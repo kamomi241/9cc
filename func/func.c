e9414db15c69db9bb8005eeea11d102cba15416a
kamomi <kamomi@mii.codes>
2022-11-03 06:56:20 +0000

    引数

--- func/func.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int foo(int x,int y) {
-    printf("%d\n",x + y);
-}
--- func/test.s
+++
@@ -1,8 +0,0 @@
-.intel_syntax noprefix
-.globl main
-main:
-  push 3
-  push 4
-  pop rsi
-  pop rdi
-  call foo
